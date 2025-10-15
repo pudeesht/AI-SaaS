8ab976f6b3aed4f7e74b245caddb953fbe0156f9
pudeesht <samarthvaibhav01@outlook.com>
2025-10-15 18:52:04 +0530

    Home page refined

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Hero from '../components/hero'
 import AiTools from '../components/AiTools'
+import Plans from '../components/plans'
+import Footer from '../components/footer'
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <AiTools/>
+      <Plans/>
+      <Footer/>
     </div>
   )
 }
